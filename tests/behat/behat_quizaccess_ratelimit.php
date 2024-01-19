b10e51b1f184cea47c5a6996059e3875cd681a8d
Felix Di Lenarda <dilenarda@tu-berlin.de>
2024-01-19 11:21:20 +0100

    Fixing behat tests for different Moodle versions

--- tests/behat/behat_quizaccess_ratelimit.php
+++ tests/behat/behat_quizaccess_ratelimit.php
@@ -47,4 +47,25 @@ class behat_quizaccess_ratelimit extends behat_base {
         $sql = "UPDATE {quizaccess_ratelimit} SET counter = 0, timemodified = 0";
         $DB->execute($sql);
     }
+
+    /**
+     * Checks the popupwindow for differently for moodle 401 and moodle 401:.
+     * 401: "Answer the first question"
+     * 402: "Attempt quiz"
+     *
+     * @Given /^I check the quiz popup window depending on Moodle version$/
+     */
+    public function i_check_the_quiz_popup_window_depending_on_moodle_version() {
+        global $CFG;
+        $branch = $CFG->branch;
+
+        if ($branch === '401') {  // If moodle 401 check "I should see "Answer the first question"
+            $this->assertSession()->pageTextContains("Answer the first question");
+        } elseif ($branch === '402') { // If moodle 402 check "I should see "Attempt quiz"
+            $this->assertSession()->pageTextContains("Attempt quiz");
+        } else {
+            // Accept the popup window but raise warning: 'The popup was not checked because of the Moodle Version'
+            $this->assertTrue(true, 'Der Popup-Check wurde aufgrund der Moodle-Version nicht durchgeführt.');
+        }
+    }
 }
